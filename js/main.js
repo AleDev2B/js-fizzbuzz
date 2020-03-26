ee4c54e1f553a2ac0c979f05b912064649aef375
Alessandro <alecritelli@gmail.com>
2020-03-26 14:40:08 +0100

    complete

--- js/main.js
+++ js/main.js
@@ -6,28 +6,28 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
 
 
-// creazione variabili per memorizzare i numeri e quelli oggetto di operazioni
-var Num, NumDiv3, NumDiv5 ;
-NumDiv3 = (Num%3==0)
+// creazione variabile Num per memorizzare un numero oggetto di un'operazione
+var Num
 
-// crea ciclo che inizia da 1 a 100 & che aumenta di una unità
+// crea ciclo che inizia da 1 a 100 e che aumenta di una unità ognivolta
 for (var Num = 1; Num <= 100; Num++) {
 
-// se il numero è un multiplo di 3 (ovvero il modulo di 3 da resto 0) la variabile numero deve stampare "Fizz"
-if (Num%3==0) {
+// se il numero è un multiplo di 3 (ovvero il modulo di 3 da resto 0) e di cinque (idem), devi stampare la parola "FizzBuzz"
+if ((Num%3 || Num%5) ==0) {
+  console.log("FizzBuzz");
+}
 
-  console.log("Fizz");
+// altrimenti se il numero è modulo di 3 devi stampare la parola "Fizz"
+else if (Num%3 ==0) {
+ console.log("Fizz");
 }
 
  // altrimenti se il numero è un modulo di 5 (quindi il modulo da resto 0) deve stampare "Buzz"
 else if (Num%5==0) {
   console.log("Buzz");
 }
- // AND se il numero è modulo di 3 + modulo di 5 allora deve stampare "FizzBuzz"
-else if (Num%3 == 0 & Num%5 == 0) {
-  console.log("FizzBuzz");
-}
-// in caso contrario deve stampare (o restituire) solo il numero.
+
+// altrimenti nel caso in cui non rientri nella condizioni di cui sopra deve stampare solo il numero a video.
 else {
   console.log(Num);
 }
