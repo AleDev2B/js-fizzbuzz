84b2b0db84fb020623f94f03e817edc55a312ab2
Alessandro <alecritelli@gmail.com>
2020-03-26 12:45:39 +0100

    FizzBuzz test
    Stamp and Operazione su 3 e 5 (ok) da effettuare qualche mod.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// DESCRIZIONE:
+// Fizz Buzz test:
+//  Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
+
+
+// creazione variabili per memorizzare i numeri e quelli oggetto di operazioni
+var Num, NumDiv3, NumDiv5 ;
+
+
+// stampare la variabile Num da 1 a 100 (invece di scrivere la riga 100 volte creo un ciclo inizializzato da 1 a 100 che aumenta a passi di 1)
+for (var Num = 1; Num <= 100; Num++) {
+  console.log(Num);
+
+// se il numero è un multiplo di 3 (ovvero il modulo di 3 da resto che è un numero intero) la variabile numero deve stampare "Fizz"
+if (Num%3==0) {
+
+  console.log("Fizz");
+
+}
+
+ // altrimenti se il numero è un modulo di 5 (quindi il modulo da resto) deve stampare "Buzz"
+else if (Num%5==0) {
+  console.log("Buzz");
+}
+ // AND se il numero è modulo di 3 + modulo di 5 allora deve stampare "FizzBuzz"
+else if (Num%3 == 0 && Num%5 == 0) {
+  console.log("FizzBuzz");
+}
+// in caso contrario deve stampare (o restituire) solo il numero.
+else {
+}
+
+}
