229f0e4cfc9297c5f04e374a1eec83e375e3aeaf
Alessandro <alecritelli@gmail.com>
2020-03-26 14:07:02 +0100

    fix comment

--- js/main.js
+++ js/main.js
@@ -10,21 +10,21 @@
 var Num, NumDiv3, NumDiv5 ;
 NumDiv3 = (Num%3==0)
 
-// stampare la variabile Num da 1 a 100 (invece di scrivere la riga 100 volte creo un ciclo inizializzato da 1 a 100 che aumenta a passi di 1)
+// crea ciclo che inizia da 1 a 100 & che aumenta di una unità
 for (var Num = 1; Num <= 100; Num++) {
 
-// se il numero è un multiplo di 3 (ovvero il modulo di 3 da resto che è un numero intero) la variabile numero deve stampare "Fizz"
+// se il numero è un multiplo di 3 (ovvero il modulo di 3 da resto 0) la variabile numero deve stampare "Fizz"
 if (Num%3==0) {
 
   console.log("Fizz");
 }
 
- // altrimenti se il numero è un modulo di 5 (quindi il modulo da resto) deve stampare "Buzz"
+ // altrimenti se il numero è un modulo di 5 (quindi il modulo da resto 0) deve stampare "Buzz"
 else if (Num%5==0) {
   console.log("Buzz");
 }
  // AND se il numero è modulo di 3 + modulo di 5 allora deve stampare "FizzBuzz"
-else if (Num%3 == 0 && Num%5 == 0) {
+else if (Num%3 == 0 & Num%5 == 0) {
   console.log("FizzBuzz");
 }
 // in caso contrario deve stampare (o restituire) solo il numero.
